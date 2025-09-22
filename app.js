d8b6de2305ec1c8409b4ae1dcb518bcecb3d1f0b
Andres Llanten <andresfllanten@gmail.com>
2025-09-22 13:16:48 -0500

    Actualizacion array de amigos

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ function agregarAmigo() {
     //Captura del valor del campo de entrada con el id amigo
   let nombreAmigo =  document.querySelector('#amigo').value;
 
-    //Condicion de campo va
+    //Validacion campo de entrada
   if (nombreAmigo != '') {
-    alert('Campo no vacio');
+    //Actualizacion array de amigos
+    amigos.push(nombreAmigo);
+    console.log(amigos);
   } else {
     alert("Por favor, inserte un nombre.");
   }
