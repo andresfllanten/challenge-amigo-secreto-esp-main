a0752678cc1d359be3be326d10b216580831a6d2
Andres Llanten <andresfllanten@gmail.com>
2025-09-22 13:08:44 -0500

    Captura del valor del campo de entrada

--- app.js
+++ app.js
@@ -2,3 +2,8 @@
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = [];
+
+function agregarAmigo() {
+    //Captura del valor del campo de entrada con el id amigo
+  let nombreAmigo =  document.querySelector('#amigo').value;
+}
