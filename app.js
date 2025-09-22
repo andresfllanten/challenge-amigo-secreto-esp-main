94338145a117588184d5f68c6b17783418232c21
Andres Llanten <andresfllanten@gmail.com>
2025-09-22 13:13:09 -0500

    Validacion del campo de entrada

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ let amigos = [];
 function agregarAmigo() {
     //Captura del valor del campo de entrada con el id amigo
   let nombreAmigo =  document.querySelector('#amigo').value;
+
+    //Condicion de campo va
+  if (nombreAmigo != '') {
+    alert('Campo no vacio');
+  } else {
+    alert("Por favor, inserte un nombre.");
+  }
 }
